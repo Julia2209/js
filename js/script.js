4c917b3f41f6e86139eb6ac01b4766adad98247d
Julia Kharchenko <jullia.kharchenko@gmail.com>
2016-07-05 16:37:02 +0300

    Add JS

--- js/script.js
+++ js/script.js
@@ -8,4 +8,30 @@ $(document).ready(function(){
 		$(this).next().slideToggle();
 	});
 
+	$(window).scroll(function(){
+
+		var pxTop = $(this).scrollTop();
+
+		if(pxTop > 170){
+			$(".hidder-img").fadeIn(500);
+
+			$(".main-menu").animate({
+				marginTop: -50,
+			},200);	
+
+		}
+		else{
+			$(".hidder-img").fadeOut(500);
+
+			var marginTopCss = $(".main-menu").css("margin-top");
+
+			if(marginTopCss == "-50px"){	
+				$(".main-menu").animate({
+					marginTop: 0,
+
+				},200);	
+			};
+		};
+	});
+
 });
